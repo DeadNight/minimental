9adb69edc93ef73635380ac71b4c1ee4c12b2e8b
nirleibo <nir.leibo@gmail.com>
2015-02-02 21:45:49 +0200

    - reformat time orientation test

--- frontend/src/test.js
+++ frontend/src/test.js
@@ -1,11 +1,48 @@
 (function() {
   var currentQuestion = 0;
+  var answers = [];
+
   var questions = [
     {
       question: 'Time orientation',
-      template: timeOrientationTemplate,
-      logic: timeOrientationLogic,
-      result: timeOrientationResult
+      template: function() {
+        var parts = [
+          'Day: <input id="day" type="number" value="1" min="1" max="31"/> ',
+          'Month: <input id="month" type="number" value="1" min="1" max="12"/>',
+          'Year: <input id="year" type="number" value="1900" min="1900" max="2100"/>',
+          'Season: <select id="season">' +
+              '<option>-</option>' +
+              '<option>Spring</option>' +
+              '<option>Summer</option>' +
+              '<option>Fall</option>' +
+              '<option>Winter</option>' +
+              '</select>'
+        ];
+        return parts.join(' ');
+      },
+      logic: function($questionBody) {
+        var $day = $questionBody.find('#day');
+        var $month = $questionBody.find('#month');
+        var $year = $questionBody.find('#year');
+
+
+        $month.change(function() {
+          var max = moment({ year: $year.val(), month: $month.val()-1 }).daysInMonth();
+          $day.attr('max', max);
+          $day.val(Math.min(max, $day.val()));
+        });
+      },
+      result: function($questionBody) {
+        var $day = $questionBody.find('#day');
+        var $month = $questionBody.find('#month');
+        var $year = $questionBody.find('#year');
+        var $season = $questionBody.find('#season');
+
+        return {
+          date: moment({ year: $year.val(), month: $month.val()-1, day: $day.val() }).toDate(),
+          season: $season.val()
+        };
+      }
     },
     {
       question: 'Place orientation',
@@ -27,12 +64,9 @@
           country: $country.val(),
           floor: $floor.val()
         };
-
       }
     }
   ];
-
-  var answers = [];
 
   $(function() {
     showQuestion(currentQuestion);
@@ -59,46 +93,5 @@
         console.log(answers);
       }
     });
-  }
-
-  function timeOrientationTemplate() {
-    var parts = [
-      'Day: <input id="day" type="number" value="1" min="1" max="31"/> ',
-      'Month: <input id="month" type="number" value="1" min="1" max="12"/>',
-      'Year: <input id="year" type="number" value="1900" min="1900" max="2100"/>',
-      'Season: <select id="season">' +
-          '<option>-</option>' +
-          '<option>Spring</option>' +
-          '<option>Summer</option>' +
-          '<option>Fall</option>' +
-          '<option>Winter</option>' +
-          '</select>'
-    ];
-    return parts.join(' ');
-  }
-
-  function timeOrientationLogic($questionBody) {
-    var $day = $questionBody.find('#day');
-    var $month = $questionBody.find('#month');
-    var $year = $questionBody.find('#year');
-
-
-    $month.change(function() {
-      var max = moment({ year: $year.val(), month: $month.val()-1 }).daysInMonth();
-      $day.attr('max', max);
-      $day.val(Math.min(max, $day.val()));
-    });
-  }
-
-  function timeOrientationResult($questionBody) {
-    var $day = $questionBody.find('#day');
-    var $month = $questionBody.find('#month');
-    var $year = $questionBody.find('#year');
-    var $season = $questionBody.find('#season');
-
-    return {
-      date: moment({ year: $year.val(), month: $month.val()-1, day: $day.val() }).toDate(),
-      season: $season.val()
-    };
   }
 })();
